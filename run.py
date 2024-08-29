6b9f700d7d6d67583a07a50a42638289e04e904b
VaibhavKr9 <vaikr99@gmail.com>
2024-08-30 03:26:19 +0530

    git fetch in Run.py

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 import subprocess
 import argparse
 
-def parserInit() -> argparse.ArgumentParser:
+def getArgs() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("-b","--branch", default="main")
     parser.add_argument("-f","--file", default="main.py")
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    parser : argparse.ArgumentParser = parserInit()
+    args : argparse.Namespace = getArgs()
 
-    subprocess.run(["git","checkout","-f","origin/" + parser.branch])
-    subprocess.run(["python",parser.file])
+    subprocess.run(["git","fetch"])
+    subprocess.run(["git","checkout","-f","origin/" + args.branch])
+    subprocess.run(["python",args.file])
